c10435f6762ac7773c03fe604dfc9b8a049a7541
Alejandro <gutierrezcrucesalejandro@gmail.com>
2024-01-15 20:15:54 +0100

    AGS Output JPA entity done

--- src/main/java/com/iesfranciscodelosrios/model/entity/Output.java
+++ src/main/java/com/iesfranciscodelosrios/model/entity/Output.java
@@ -1,15 +1,36 @@
 package com.iesfranciscodelosrios.model.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.UUID;
 
 @Data
 @EqualsAndHashCode
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
+@Entity
+@Table(name = "outputs")
 public class Output {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "formula")
     private String formula;
+
+    @ManyToOne
+    @JoinColumn(name = "output_id")
+    private UserEntity userOwner;
+
+    @Column(name = "result")
+    private String result;
 }
