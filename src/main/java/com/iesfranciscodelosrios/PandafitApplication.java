b34e1595defe3b980eed7251ea47e689e0080ece
najocar <josecarloscastrolopez@gmail.com>
2024-01-15 10:05:23 +0100

    proyect

---
+++ src/main/java/com/iesfranciscodelosrios/PandafitApplication.java
@@ -0,0 +1,13 @@
+package com.iesfranciscodelosrios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandafitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandafitApplication.class, args);
+	}
+
+}
