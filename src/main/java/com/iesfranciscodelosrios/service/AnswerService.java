52929d1917531e442e04e13e5ef06a4cf1e82fcc
Alejandro <gutierrezcrucesalejandro@gmail.com>
2024-01-16 13:02:04 +0100

    AGS AnswerService done with interface

--- src/main/java/com/iesfranciscodelosrios/service/AnswerService.java
+++ src/main/java/com/iesfranciscodelosrios/service/AnswerService.java
@@ -1,15 +1,17 @@
 package com.iesfranciscodelosrios.service;
 
 import com.iesfranciscodelosrios.model.entity.Answer;
+import com.iesfranciscodelosrios.model.interfaze.iServices;
 import com.iesfranciscodelosrios.repository.AnswerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
-public class AnswerService{
+public class AnswerService implements iServices<Answer> {
     @Autowired
     AnswerRepository answerRepository;
 
@@ -18,12 +20,25 @@ public class AnswerService{
         return answer.orElse(null);
     }
 
-    public Answer saveAnswer(Answer answer) {
+    public void deleteAnswer(Answer answer) {
+
+    }
+
+    @Override
+    public Answer findById(UUID id) {
+        Optional<Answer> answer = answerRepository.findById(id);
+        return answer.orElse(null);
+    }
+
+    @Override
+    public Answer save(Answer answer) {
         return answerRepository.save(answer);
     }
 
-    public void deleteAnswer(Answer answer) {
+    @Override
+    public Answer delete(Answer answer) {
         answerRepository.delete(answer);
+        return answer;
     }
 }
 
