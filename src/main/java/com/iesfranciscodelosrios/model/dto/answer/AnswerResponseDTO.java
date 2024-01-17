0730d7e17a512996b676f4249ddbcf9721e4c220
Alejandro <gutierrezcrucesalejandro@gmail.com>
2024-01-17 20:59:09 +0100

    AGS Creation of Answer controller and DTO's

---
+++ src/main/java/com/iesfranciscodelosrios/model/dto/answer/AnswerResponseDTO.java
@@ -0,0 +1,18 @@
+package com.iesfranciscodelosrios.model.dto.answer;
+
+import com.iesfranciscodelosrios.model.entity.FormAct;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AnswerResponseDTO {
+    private UUID id;
+    private LocalDateTime date;
+    private FormAct formAct;
+    private String uuid;
+}
