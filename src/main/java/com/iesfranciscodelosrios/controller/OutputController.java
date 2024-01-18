8a28b74eaf34564293314179a034e4ba65b1a8e7
najocar <josecarloscastrolopez@gmail.com>
2024-01-18 08:57:23 +0100

    JCC Comment output controller

--- src/main/java/com/iesfranciscodelosrios/controller/OutputController.java
+++ src/main/java/com/iesfranciscodelosrios/controller/OutputController.java
@@ -17,6 +17,11 @@ public class OutputController {
     @Autowired
     private OutputService OutputService;
 
+    /**
+     * Find by name
+     * @param name name of the output
+     * @return the output or 404
+     */
     @GetMapping("/output/name/:name")
     public ResponseEntity<OutputResponseDTO> findByName(@RequestParam("name") String name) {
         Output output = OutputService.findByName(name);
@@ -34,6 +39,11 @@ public class OutputController {
         return ResponseEntity.ok(outputResponseDTO);
     }
 
+    /**
+     * Find by id
+     * @param id id of the output
+     * @return the output or 404
+     */
     @GetMapping("/output/:id")
     public ResponseEntity<OutputResponseDTO> findById(@RequestParam("id") UUID id) {
         Output output = OutputService.findById(id);
@@ -51,6 +61,11 @@ public class OutputController {
         return ResponseEntity.ok(outputResponseDTO);
     }
 
+    /**
+     * Save an output
+     * @param outputCreateDTO the output to save without id
+     * @return the saved output with id
+     */
     @PostMapping("/output")
     public ResponseEntity<OutputResponseDTO> save(@RequestBody OutputCreateDTO outputCreateDTO) {
         Output output = OutputService.save(Output.builder()
@@ -70,6 +85,11 @@ public class OutputController {
         return ResponseEntity.ok(outputResponseDTO);
     }
 
+    /**
+     * Update an output
+     * @param outputUpdateDTO the output to update
+     * @return the updated output
+     */
     @PutMapping("/output")
     public ResponseEntity<OutputResponseDTO> update(@RequestBody OutputUpdateDTO outputUpdateDTO) {
         Output output = OutputService.save(Output.builder()
@@ -90,6 +110,11 @@ public class OutputController {
         return ResponseEntity.ok(outputResponseDTO);
     }
 
+    /**
+     * Delete an output
+     * @param outputUpdateDTO the output to delete
+     * @return the deleted output
+     */
     @DeleteMapping("/output")
     public ResponseEntity<OutputResponseDTO> delete(@RequestBody OutputUpdateDTO outputUpdateDTO) {
         Output output = OutputService.save(Output.builder()
