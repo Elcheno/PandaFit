eac3362d49304be670185f622e1f84cc768ccb95
Isabel <isabelroldancordoba@hotmail.com>
2024-01-20 14:43:58 +0100

    IRC Agregado notNull para institution y para name le he añadido un notBlank yun pattern para que sea válido, expresión regular detallada en el código

--- src/main/java/com/iesfranciscodelosrios/model/entity/SchoolYear.java
+++ src/main/java/com/iesfranciscodelosrios/model/entity/SchoolYear.java
@@ -1,6 +1,10 @@
 package com.iesfranciscodelosrios.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.Set;
@@ -20,9 +24,14 @@ public class SchoolYear {
     @EqualsAndHashCode.Include
     private UUID id;
 
+    @NotBlank(message = "El nombre no puede estar en blanco")
+    // Dígito entre 1 y 4 seguido de "º", espacio en blanco, seguido de ESO o BACHILLERATO, espacio en blanco, grupo capturador que acepta una o más palabras
+    // la cual cada una comienza con una letra mayúscula, espacio en blanco y por último una letra en mayúscula
+    @Pattern(regexp = "^[1-4]º\\s(?:ESO|BACHILLERATO)\\s(?:[A-Z]+(?:\\s[A-Z]+)?)\\s(?:[A-Z])$", message = "El formato del nombre debe ser como '1º ESO C', '2º BACHILLERATO CIENCIAS SOCIALES A', '3º BACHILLERATO CS A', '4º ESO B', etc.")
     @Column(name = "name", unique = true)
     private String name;
 
+    @NotNull(message = "La institución no puede ser nula")
     @ManyToOne
     @JoinColumn(name = "institution_id", nullable = false, unique = true)
     @EqualsAndHashCode.Exclude
