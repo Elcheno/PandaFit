a80d1535fafeb76ee6c2559b26deecd2b0bd6fe2
Isabel <isabelroldancordoba@hotmail.com>
2024-01-20 14:38:52 +0100

    IRC Agregado notBlank para name y validator y notNull para userOwner

--- src/main/java/com/iesfranciscodelosrios/model/entity/Input.java
+++ src/main/java/com/iesfranciscodelosrios/model/entity/Input.java
@@ -1,6 +1,8 @@
 package com.iesfranciscodelosrios.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.Objects;
@@ -19,16 +21,19 @@ public class Input {
     private UUID id;
 
     @Column(name = "name", unique = true)
+    @NotBlank(message = "El nombre no puede estar vacío")
     private String name;
 
     @Column(name = "description")
     private String description;
 
     @Column(name = "validator")
+    @NotBlank(message = "El validador no puede estar vacío")
     private String validator;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_owner_id", nullable = false)
+    @NotNull(message = "El propietario del usuario no puede ser nulo")
     private UserEntity userOwner;
 
     @Override
