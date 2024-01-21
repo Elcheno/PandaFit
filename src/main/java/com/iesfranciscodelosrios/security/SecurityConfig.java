2bbd7a89be4e71cf2f13d4b3ac84a954f075e437
Elcheno <r.c.a2312@gmail.com>
2024-01-21 13:24:05 +0100

    implement user in memory and authenticationManager

--- src/main/java/com/iesfranciscodelosrios/security/SecurityConfig.java
+++ src/main/java/com/iesfranciscodelosrios/security/SecurityConfig.java
@@ -2,11 +2,17 @@ package com.iesfranciscodelosrios.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,9 +32,30 @@ public class SecurityConfig {
 
     }
 
-//    @Bean
-//    PasswordEncoder passwordEncoder() {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
-//    }
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(User.withUsername("usuario")
+                .password("usuario")
+                .roles()
+                .build());
+
+        return manager;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder)
+                .and().build();
+    }
 
 }
