ca98e40d9539e36574ed951e86ad21f2463d9577
Elcheno <r.c.a2312@gmail.com>
2024-01-21 14:16:44 +0100

    RCA add UserDeleteDTO and implement in UserController

---
+++ src/main/java/com/iesfranciscodelosrios/model/dto/user/UserDeleteDTO.java
@@ -0,0 +1,34 @@
+package com.iesfranciscodelosrios.model.dto.user;
+
+import com.iesfranciscodelosrios.model.entity.Form;
+import com.iesfranciscodelosrios.model.entity.Input;
+import com.iesfranciscodelosrios.model.entity.Institution;
+import com.iesfranciscodelosrios.model.entity.Output;
+import com.iesfranciscodelosrios.model.type.RoleType;
+import lombok.*;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class UserDeleteDTO {
+    private UUID id;
+
+    private String email;
+
+    private String password;
+
+    private Institution institution;
+
+    private Set<RoleType> role;
+
+    private Set<Input> inputList;
+
+    private Set<Output> outputList;
+
+    private Set<Form> formList;
+}
