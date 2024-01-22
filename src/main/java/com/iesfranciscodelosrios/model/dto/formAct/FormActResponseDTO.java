e25793afdf0e6b441d7468cf6b786721ac3a0242
Ramon Romero <ramonromeca@gmail.com>
2024-01-22 12:22:08 +0100

    RRC created FormAct controller and its DTOs

---
+++ src/main/java/com/iesfranciscodelosrios/model/dto/formAct/FormActResponseDTO.java
@@ -0,0 +1,32 @@
+package com.iesfranciscodelosrios.model.dto.formAct;
+
+import com.iesfranciscodelosrios.model.entity.Answer;
+import com.iesfranciscodelosrios.model.entity.Form;
+import com.iesfranciscodelosrios.model.entity.SchoolYear;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class FormActResponseDTO {
+
+    private UUID id;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime expirationDate;
+
+    private Form form;
+
+    private SchoolYear schoolYear;
+
+    private Set<Answer> answersList;
+
+}
+
