5616d410d26a312c23b571931ada9b09c20ec063
Elcheno <r.c.a2312@gmail.com>
2024-01-22 21:15:31 +0100

    RCA add method to findAllByInstitution in SchoolYearRepository

--- src/main/java/com/iesfranciscodelosrios/repository/SchoolYearRepository.java
+++ src/main/java/com/iesfranciscodelosrios/repository/SchoolYearRepository.java
@@ -3,6 +3,9 @@ package com.iesfranciscodelosrios.repository;
 
 import com.iesfranciscodelosrios.model.entity.Institution;
 import com.iesfranciscodelosrios.model.entity.SchoolYear;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +13,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface SchoolYearRepository extends CrudRepository<SchoolYear, UUID> {
+public interface SchoolYearRepository extends JpaRepository<SchoolYear, UUID> {
 
     Optional<SchoolYear> findByNameAndAndInstitution(String name, Institution institution);
+
+    Page<SchoolYear> findAllByInstitution(Institution institution, Pageable pageable);
 }
