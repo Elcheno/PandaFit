897b03bfbbb580d0f49c5c8acac603cccd193493
Elcheno <r.c.a2312@gmail.com>
2024-01-22 18:07:14 +0100

    RCA add method findAll in InstitutionService

--- src/main/java/com/iesfranciscodelosrios/service/InstitutionService.java
+++ src/main/java/com/iesfranciscodelosrios/service/InstitutionService.java
@@ -6,6 +6,8 @@ import com.iesfranciscodelosrios.repository.InstitutionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -34,5 +36,15 @@ public class InstitutionService implements iServices<Institution> {
     public Institution findByName(String name) {
         return institutionRepository.findByName(name)
                 .orElse(null);
+    }
+
+    public Set<Institution> findAll() {
+        Set<Institution> result = new HashSet<>();
+
+        institutionRepository.findAll().forEach(institution -> {
+            result.add(institution);
+        });
+
+        return result;
     }
 }
