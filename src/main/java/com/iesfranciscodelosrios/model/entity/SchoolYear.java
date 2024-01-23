2d0fc8a49479be7e6afe6be6a5149c3312385d05
Isabel <isabelroldancordoba@hotmail.com>
2024-01-23 08:47:33 +0100

    IRC Fixed pattern to control the SchoolYear name. Now more simplified

--- src/main/java/com/iesfranciscodelosrios/model/entity/SchoolYear.java
+++ src/main/java/com/iesfranciscodelosrios/model/entity/SchoolYear.java
@@ -27,7 +27,7 @@ public class SchoolYear {
     @NotBlank(message = "El nombre no puede estar en blanco")
     // Dígito entre 1 y 4 seguido de "º", espacio en blanco, seguido de ESO o BACHILLERATO, espacio en blanco, grupo capturador que acepta una o más palabras
     // la cual cada una comienza con una letra mayúscula, espacio en blanco y por último una letra en mayúscula
-//    @Pattern(regexp = "^[1-4]º\\s(?:ESO|BACHILLERATO)\\s(?:[A-Z]+(?:\\s[A-Z]+)?)\\s(?:[A-Z])$", message = "El formato del nombre debe ser como '1º ESO C', '2º BACHILLERATO CIENCIAS SOCIALES A', '3º BACHILLERATO CS A', '4º ESO B', etc.")
+    @Pattern(regexp = "^[A-Za-z0-9]{2,25}$", message = " Debe haber entre 2 y 25 dígitos")
     @Column(name = "name", unique = true)
     private String name;
 
