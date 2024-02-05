271c2f3d57b7a090273ed161351adc8abf88ebd5
najocar <josecarloscastrolopez30@gmail.com>
2024-02-05 13:05:00 +0100

    JCC FixSchoolYearDelete

--- src/main/java/com/iesfranciscodelosrios/repository/SchoolYearRepository.java
+++ src/main/java/com/iesfranciscodelosrios/repository/SchoolYearRepository.java
@@ -6,6 +6,8 @@ import com.iesfranciscodelosrios.model.entity.SchoolYear;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -18,4 +20,8 @@ public interface SchoolYearRepository extends JpaRepository<SchoolYear, UUID> {
     Optional<SchoolYear> findByNameAndAndInstitution(String name, Institution institution);
 
     Page<SchoolYear> findAllByInstitution(Institution institution, Pageable pageable) throws Exception;
+
+    @Modifying
+    @Query(value = "DELETE FROM school_year as u WHERE u.id = ?1", nativeQuery = true)
+    void forceDelete(UUID sachoolYearID) throws Exception;
 }
