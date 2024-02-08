a887fcf6b97ca1e86f3fe9652f6cec6dd36a21a8
Isabel <isabelroldancordoba@hotmail.com>
2024-02-08 13:12:52 +0100

    Modificación de Input con todas sus clases relacionadas, adaptandolas a las nuevas necesidades

--- src/main/java/com/iesfranciscodelosrios/model/dto/input/InputUpdateDTO.java
+++ src/main/java/com/iesfranciscodelosrios/model/dto/input/InputUpdateDTO.java
@@ -1,6 +1,7 @@
 package com.iesfranciscodelosrios.model.dto.input;
 
 import com.iesfranciscodelosrios.model.entity.UserEntity;
+import com.iesfranciscodelosrios.model.type.TypeType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +15,16 @@ import java.util.UUID;
 @Builder
 public class InputUpdateDTO {
     private UUID id;
+
     private String name;
+
     private String description;
-    private String validator;
+
+    private TypeType type;
+
+    private Boolean decimal;
+
+    private Number decimals;
+
+    private String unit;
 }
---
+++ src/main/java/com/iesfranciscodelosrios/model/type/TypeType.java
@@ -0,0 +1,2 @@
+package com.iesfranciscodelosrios.model.type;public enum TypeType {
+}
