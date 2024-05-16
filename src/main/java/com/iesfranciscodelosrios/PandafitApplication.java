a828f78a8bd8ba71ccfd73cf9bbcd42c836b6166
RubenKiconex <ruben.kiconex@gmail.com>
2024-05-16 19:38:08 +0200

    RCA implement war build

--- src/main/java/com/iesfranciscodelosrios/PandafitApplication.java
+++ src/main/java/com/iesfranciscodelosrios/PandafitApplication.java
@@ -2,12 +2,18 @@ package com.iesfranciscodelosrios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PandafitApplication {
+public class PandafitApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PandafitApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PandafitApplication.class);
+	}
 }
